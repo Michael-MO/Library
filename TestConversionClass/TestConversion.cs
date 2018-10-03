a501af2309deab980939b5c3f89a6d067e9db5d1
Michael-MO <Michael-MO@users.noreply.github.com>
2018-10-03 13:44:40 +0200

    Add more tests

--- TestConversionClass/TestConversion.cs
+++ TestConversionClass/TestConversion.cs
@@ -13,6 +13,17 @@ namespace TestConversionClass
         }
 
         [TestMethod]
+        public void ConvertOneOunceToGram2()
+        {
+            Assert.AreNotEqual(30, Conversion.ConvertToGrams(1), 0.1);
+        }
+
+        public void ConvertOneOunceToGram3()
+        {
+            Assert.AreNotEqual(1, Conversion.ConvertToGrams(1), 0.1);
+        }
+
+        [TestMethod]
         public void ConvertOneGramToOunce()
         {
             Assert.AreEqual(0.03527396195, Conversion.ConvertToOunces(1), 0.1);
