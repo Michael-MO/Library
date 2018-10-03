14c27ed53f7966ef68c289af9907ec3a9ade0d59
Michael-MO <Michael-MO@users.noreply.github.com>
2018-10-03 12:07:35 +0200

    Initial commit

---
+++ ConversionClass/Conversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConversionClass
+{
+    public static class Conversion
+    {
+        private static double CONSTANT = 28.34952;
+
+        public static double ConvertToOunces(double grams)
+        {
+            return grams / CONSTANT;
+        }
+
+        public static double ConvertToGrams(double ounces)
+        {
+            return ounces * CONSTANT;
+        }
+    }
+}
---
+++ TestConversionClass/TestConversion.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConversionClass;
+
+namespace TestConversionClass
+{
+    [TestClass]
+    public class TestConversion
+    {
+        [TestMethod]
+        public void ConvertOneOunceToGram()
+        {
+            Assert.AreEqual(28.34952, Conversion.ConvertToGrams(1), 0.1);
+        }
+
+        [TestMethod]
+        public void ConvertOneGramToOunce()
+        {
+            Assert.AreEqual(0.03527396195, Conversion.ConvertToOunces(1), 0.1);
+        }
+    }
+}
